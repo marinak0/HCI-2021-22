1b5b00e68935b610efb023fbb854e0192e75dc44
marinak0 <marina.kovacevic55@gmail.com>
2021-11-03 15:55:08 +0100

    switched branches

--- modules/header/header.js
+++ modules/header/header.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React from 'react';
+import styles from './header.module.css';
+import Image from 'next/image';
+
+import LogoImg from '../../assets/logo.png';
+import HeroImg from '../../assets/hero.png';
 
 const Header = () => {
-  return (
-    <header>
-      <div>Logo</div>
-      <nav>
-        <li>Home</li>
-        <li>About us</li>
-        <li>Showcase</li>
-        <li>Blog</li>
-        <li>Contact</li>
-        <li>Sign in</li>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={styles.main}>
+            <div className={styles.bgImg}>
+                <Image
+                    src={HeroImg}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Hero image"
+                />
+            </div>
+            <main className={styles.content}>
+                <section className={styles.navigation}>
+                    <Image
+                        src={LogoImg}
+                        layout="fixed"
+                        width={150}
+                        height={150}
+                        alt="Design logo"
+                    />
+                    <nav>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Showcase</li>
+                        <li>Blog</li>
+                        <li>Contact</li>
+                        <li>Sign In</li>
+                    </nav>
+                </section>
+                <section className={styles.titles}>
+                    <h1>Design Matters</h1>
+                    <h3>Entrust us with your digital appearance</h3>
+                </section>
+            </main>
+        </header>
+    );
 };
+
 export default Header;
